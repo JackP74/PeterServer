5cff05f92d0e25ab9ad56dd3d8b5d7d14bd999f1
JackP74 <live.a.life74@gmail.com>
2025-08-13 16:34:34 +0300

    initial client side

---
+++ PeterClient/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace PeterClient
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ PeterClient/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace PeterClient
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
